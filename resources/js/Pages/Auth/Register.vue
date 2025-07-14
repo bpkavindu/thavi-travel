7ccfcb5bf2d9d3e1611ae5c3c41332cc22794fa8
kavindu <bpkavindu@gmail.com>
2025-07-14 13:05:45 +0530

    test

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -26,7 +26,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="name" value="Name 1" />
+                <InputLabel for="name" value="Name" />
 
                 <TextInput
                     id="name"
