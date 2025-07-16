a7e02357fb57eba7b0cb547ff6943515a04ec818
kavindu <bpkavindu@gmail.com>
2025-07-17 00:35:34 +0530

    rename

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="min-h-screen bg-gray-100">
     <header class="bg-white shadow p-4">
-      <h1 class="text-2xl font-bold text-gray-800">Creative Dashboard</h1>
+      <h1 class="text-2xl font-bold text-gray-800">Dashboard</h1>
     </header>
     <main class="p-6">
       <slot />
